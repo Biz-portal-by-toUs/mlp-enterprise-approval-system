749d2b8a91e4827005b21ca4aa9500d822ea6b12
Lee Ji Heon <jiheon340@gmail.com>
2025-12-14 13:36:27 +0900

    feat: Initial project setup

---
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/MlpEnterpriseApprovalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.multi.mlpenterpriseapprovalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlpEnterpriseApprovalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlpEnterpriseApprovalSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multi/mlpenterpriseapprovalsystem/MlpEnterpriseApprovalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.mlpenterpriseapprovalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlpEnterpriseApprovalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
