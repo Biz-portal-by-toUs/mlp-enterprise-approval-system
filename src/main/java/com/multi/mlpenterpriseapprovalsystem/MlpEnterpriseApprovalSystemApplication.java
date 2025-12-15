f4817f69ac319a85a0b7cd317bdac87f716f8508
kimyoungkwan <kebkwan@gmail.com>
2025-12-15 10:24:06 +0900

    test

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/MlpEnterpriseApprovalSystemApplication.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/MlpEnterpriseApprovalSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MlpEnterpriseApprovalSystemApplication {
-
+      // test
     public static void main(String[] args) {
         SpringApplication.run(MlpEnterpriseApprovalSystemApplication.class, args);
     }
