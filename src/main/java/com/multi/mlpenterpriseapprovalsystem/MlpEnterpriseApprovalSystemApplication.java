7f50df3dc4dcd9c4229c570ddf119145b7ec743c
Lee Ji Heon <jiheon340@gmail.com>
2025-12-15 10:35:30 +0900

    origin main push test

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/MlpEnterpriseApprovalSystemApplication.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/MlpEnterpriseApprovalSystemApplication.java
@@ -3,6 +3,7 @@ package com.multi.mlpenterpriseapprovalsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// test
 @SpringBootApplication
 public class MlpEnterpriseApprovalSystemApplication {
 
