b625144b85808a75c860daa7de8c5c7393832ee0
seunggi-k <fdvc10@gmail.com>
2025-12-17 00:03:19 +0900

    채팅 도메인 및 기본 메시지 구조 설계

---
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/chat/domain/MessageType.java
@@ -0,0 +1,12 @@
+package com.multi.mlpenterpriseapprovalsystem.chat.domain;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : 김승기
+ * @filename : MessageType
+ * @since : 2025. 12. 16. 화요일
+ */
+public enum MessageType {
+    TEXT, IMAGE, SYSTEM
+}
