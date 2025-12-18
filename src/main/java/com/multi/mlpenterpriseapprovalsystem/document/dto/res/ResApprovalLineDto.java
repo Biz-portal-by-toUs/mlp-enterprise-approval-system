97aef165175995cbada26bff8eceac28eebbee8c
Lee Ji Heon <jiheon340@gmail.com>
2025-12-18 14:46:29 +0900

    feat: 결제라인 반환 Dto 생성

---
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/document/dto/res/ResApprovalLineDto.java
@@ -0,0 +1,47 @@
+package com.multi.mlpenterpriseapprovalsystem.document.dto.res;
+
+import com.multi.mlpenterpriseapprovalsystem.document.domain.ApprovalLine;
+import com.multi.mlpenterpriseapprovalsystem.document.enums.ApprStat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : 이지헌
+ * @filename : ResApprovalLineDto
+ * @since : 25. 12. 18. 목요일
+ */
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResApprovalLineDto {
+    private Long apprlNo;
+    private String docNo;
+    private String approverName; // 결재자 이름
+    private String approverId; // 결재자 사원번호
+    private String comId;
+    private int seq;
+    private ApprStat apprStat;
+    private LocalDateTime endedAt;
+
+    public static ResApprovalLineDto toDto(ApprovalLine approvalLine) {
+        return ResApprovalLineDto.builder()
+                .apprlNo(approvalLine.getApprlNo())
+                .docNo(approvalLine.getDocument().getDocId())
+                .approverName(approvalLine.getApprover().getEmpName())
+                .approverId(approvalLine.getApprover().getEmpId())
+                .comId(approvalLine.getCompany().getComId())
+                .seq(approvalLine.getSeq())
+                .apprStat(approvalLine.getApprStat())
+                .endedAt(approvalLine.getEndedAt())
+                .build();
+    }
+
+}
