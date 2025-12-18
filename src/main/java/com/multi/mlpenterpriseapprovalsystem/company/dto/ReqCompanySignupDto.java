bf2def8c2a2ca68f6829efcde4d322fe0232e026
ziyoungkwon <gy011003@naver.com>
2025-12-19 00:17:12 +0900

    feat: 기업 회원가입 요청 dto 필드 수정

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/company/dto/ReqCompanySignupDto.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/company/dto/ReqCompanySignupDto.java
@@ -7,14 +7,13 @@ package com.multi.mlpenterpriseapprovalsystem.company.dto;
  * @filename : CompanyReqDto
  * @since : 2025. 12. 17. 수요일
  */
-public class ReqCompanyDto {
+public class ReqCompanySignupDto {
 
     private String comId;
     private String comName;
     private String email;
     private String pwd;
     private String brn;
-    private String address;
-    private String imgUrl;
-    private String path;
+    private String addr;
+    private Integer subNo;
 }
