66688786f3cf7aee7246166d3628758545cc590e
Songhyun Go <zhthd201@gmail.com>
2025-12-19 23:42:02 +0900

    feat: Dto에 validation 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/meetingroom/dto/ReqMeetingRoomDto.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/meetingroom/dto/ReqMeetingRoomDto.java
@@ -1,5 +1,9 @@
 package com.multi.mlpenterpriseapprovalsystem.reservation.meetingroom.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 /**
@@ -16,11 +20,24 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ReqMeetingRoomDto {
+
+    @NotBlank(message = "회의실명은 필수입니다.")
+    @Size(max = 20, message = "회의실명은 20자 이내여야 합니다.")
     private String roomName;
+
+    @NotNull(message = "수용 인원은 필수입니다.")
+    @Min(value = 1, message = "수용 인원은 1명 이상이어야 합니다.")
     private Integer capacity;
+
+    @NotBlank(message = "위치는 필수입니다.")
     private String location;
+
     private String imageUrl;
+
+    @Size(max = 255)
     private String equipList;
+
+    @Size(max = 255)
     private String note;
 
 }
