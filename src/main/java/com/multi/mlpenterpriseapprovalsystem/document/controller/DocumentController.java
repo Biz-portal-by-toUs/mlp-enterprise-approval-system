6c13b09c58c19253a862bacc831aa3f9a026e914
Lee Ji Heon <jiheon340@gmail.com>
2025-12-20 17:13:40 +0900

    feat: 문서 조회에 정렬기능 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/document/controller/DocumentController.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/document/controller/DocumentController.java
@@ -2,6 +2,8 @@ package com.multi.mlpenterpriseapprovalsystem.document.controller;
 
 import com.multi.mlpenterpriseapprovalsystem.auth.dto.CustomUser;
 import com.multi.mlpenterpriseapprovalsystem.common.ResponseDto;
+import com.multi.mlpenterpriseapprovalsystem.common.exception.CustomException;
+import com.multi.mlpenterpriseapprovalsystem.common.exception.ErrorCode;
 import com.multi.mlpenterpriseapprovalsystem.document.dto.res.ResDocumentDto;
 import com.multi.mlpenterpriseapprovalsystem.document.service.DocumentService;
 import lombok.RequiredArgsConstructor;
@@ -31,15 +33,22 @@ public class DocumentController {
 
     private final DocumentService documentService;
 
-    // 최종승인된 문서 조회
+    // 회사 전체의 문서를 문서상태 기준 조회
     @GetMapping("/documents")
-    public ResponseEntity<ResponseDto<Page<ResDocumentDto>>> getApprovedDocuments(@AuthenticationPrincipal CustomUser customUser,
+    public ResponseEntity<ResponseDto<Page<ResDocumentDto>>> getDocumentsByStatus(@AuthenticationPrincipal CustomUser customUser,
                                                                                   @RequestParam(name = "status") String status,
-                                                                                  @RequestParam(name = "page", defaultValue = "0") int page) {
+                                                                                  @RequestParam(name = "page", defaultValue = "0") int page,
+                                                                                  @RequestParam(name = "sort", defaultValue = "LATEST") String sort) {
 
-        Page<ResDocumentDto> resDocumentDtos = documentService.getDocumentsByStatus(customUser.getComId(), customUser.getUsername(), status, page);
+        Page<ResDocumentDto> resDocumentDtos = documentService.getDocumentsByStatus(customUser.getComId(), customUser.getUsername(), status, page, sort);
 
-        String message = resDocumentDtos.isEmpty() ? "최종 승인된 문서가 없습니다" : "최종 승인된 문서 조회 성공";
+        String message = "";
+        if("FINALIZED".equals(status)){
+            message = resDocumentDtos.isEmpty() ? "최종 승인된 문서가 없습니다" : "최종 승인된 문서 조회 성공";
+        }
+        else{
+            throw new CustomException(ErrorCode.INVALID_DOCUMENT_STATUS_REQUEST);
+        }
 
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -48,21 +57,25 @@ public class DocumentController {
 
     // 내가 상신한 문서 조회(status = "SUBMITTED"), 내가 결재할 문서 조회(status = "AWAITING"), 내가 결재한 문서 조회(status = "PROCESSED")
     @GetMapping("/documents/me")
-    public ResponseEntity<ResponseDto<Page<ResDocumentDto>>> getMySubmittedDocuments(@AuthenticationPrincipal CustomUser customUser,
+    public ResponseEntity<ResponseDto<Page<ResDocumentDto>>> getMyDocumentsByStatus(@AuthenticationPrincipal CustomUser customUser,
                                                                                      @RequestParam(name = "status") String status,
-                                                                                     @RequestParam(name = "page", defaultValue = "0") int page){
+                                                                                     @RequestParam(name = "page", defaultValue = "0") int page,
+                                                                                     @RequestParam(name = "sort", defaultValue = "LATEST") String sort){
 
-        Page<ResDocumentDto> resDocumentDtos = documentService.getDocumentsByStatus(customUser.getComId(), customUser.getUsername(), status, page);
+        Page<ResDocumentDto> resDocumentDtos = documentService.getMyDocumentsByStatus(customUser.getComId(), customUser.getUsername(), status, page, sort);
 
         String message = "";
-        if(status.equals("SUBMITTED")) {
+        if("SUBMITTED".equals(status)) {
             message = resDocumentDtos.isEmpty() ? "내가 상신한 문서가 없습니다" : "내가 상신한 문서 조회 성공";
         }
-        else if(status.equals("AWAITING")) {
+        else if("AWAITING".equals(status)) {
             message = resDocumentDtos.isEmpty() ? "내가 결재할 문서가 없습니다" : "내가 결재할 문서 조회 성공";
         }
-        else if(status.equals("PROCESSED")) {
+        else if("PROCESSED".equals(status)) {
             message = resDocumentDtos.isEmpty() ? "내가 결재한 문서가 없습니다" : "내가 결재한 문서 조회 성공";
+        }
+        else{
+            throw new CustomException(ErrorCode.INVALID_DOCUMENT_STATUS_REQUEST);
         }
 
         return ResponseEntity
--- src/main/java/com/multi/mlpenterpriseapprovalsystem/document/repository/DocumentRepository.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/document/repository/DocumentRepository.java
@@ -1,14 +1,14 @@
 package com.multi.mlpenterpriseapprovalsystem.document.repository;
 
 import com.multi.mlpenterpriseapprovalsystem.document.domain.Document;
+import com.multi.mlpenterpriseapprovalsystem.document.enums.ApprStat;
+import com.multi.mlpenterpriseapprovalsystem.document.enums.DocStat;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 /**
  * 문서 테이블 관리 repository
@@ -20,46 +20,110 @@ import java.util.List;
 
 public interface DocumentRepository extends JpaRepository<Document, Long> {
 
-
-    @Query("SELECT DISTINCT d FROM Document d " +
-            "JOIN FETCH d.approvalLines al " +
-            "WHERE d.company.comId = :comId " +
-            "AND d.temp = false")
-    List<Document> findAllWithApprovalLinesByComId(@Param("comId") String comId);
-
-    // 내가 상신한 문서 조회
+    // 내 회사의 문서 중 내가 상신한 문서 조회
+    // 작성자가 나인 문서 반환
+    @EntityGraph(attributePaths = {"writer", "documentForm"})
     @Query("SELECT d FROM Document d " +
             "WHERE d.company.comId = :comId " +
             "AND d.writer.empId = :empId " +
             "AND d.temp = false")
-    Page<Document> getMySubmittedDocuments(@Param("comId") String comId, @Param("empId") String empId, Pageable pageable);
+    Page<Document> getMySubmittedDocuments(@Param("comId") String comId,
+                                           @Param("empId") String empId,
+                                           Pageable pageable);
 
-    // 내가 결재할 문서 조회
-    // 결재라인에 내가 있고 상태가 I인 문서들을 모두 반환해야함
+    // 내 회사의 문서 중 내가 상신한 문서 조회
+    // 작성자가 나인 문서 반환(결재중, 최종승인, 반려)
+    @EntityGraph(attributePaths = {"writer", "documentForm"})
+    @Query("SELECT d FROM Document d " +
+            "WHERE d.company.comId = :comId " +
+            "AND d.writer.empId = :empId " +
+            "AND d.temp = false " +
+            "ORDER BY CASE WHEN d.docStat = :docStat THEN 0 ELSE 1 END ASC, " +
+            "d.createdAt DESC")
+    Page<Document> getMySubmittedDocumentsByDocStat(@Param("comId") String comId,
+                                           @Param("empId") String empId,
+                                           @Param("docStat") DocStat docStat,
+                                           Pageable pageable);
+
+//======================================================================================================================
+
+    // 내 회사의 문서 중 내가 결재할 문서 조회
+    // 결재라인에 내가 있고 상태가 결재중, 결재대기중인 문서 조회(결재중이 먼저 오도록)
     @EntityGraph(attributePaths = {"writer", "documentForm"})
     @Query("SELECT d FROM Document d " + // DISTINCT 제거
             "JOIN d.approvalLines al " +
             "WHERE d.company.comId = :comId " +
-            "AND d.docStat = com.multi.mlpenterpriseapprovalsystem.document.enums.DocStat.AW " +
+            "AND d.docStat = :docStatAW " +
             "AND al.approver.empId = :empId " +
-            "AND al.apprStat IN (com.multi.mlpenterpriseapprovalsystem.document.enums.ApprStat.I, " +
-            "com.multi.mlpenterpriseapprovalsystem.document.enums.ApprStat.W) " +
-            "ORDER BY CASE WHEN al.apprStat = com.multi.mlpenterpriseapprovalsystem.document.enums.ApprStat.I THEN 0 " +
-            "              WHEN al.apprStat = com.multi.mlpenterpriseapprovalsystem.document.enums.ApprStat.W THEN 1 " +
+            "AND al.apprStat IN (:apprStatI, " + ":apprStatW) " +
+            "ORDER BY CASE WHEN al.apprStat = :apprStatI THEN 0 " +
+            "              WHEN al.apprStat = :apprStatW THEN 1 " +
             "              ELSE 2 END ASC, d.createdAt DESC")
-    Page<Document> getDocumentsAwaitingMyApproval(@Param("comId") String comId, @Param("empId") String empId, Pageable pageable);
+    Page<Document> getAwaitingMyApprovalDocuments(@Param("comId")String comId,
+                                                  @Param("empId") String empId,
+                                                  @Param("apprStatI")ApprStat apprStatI,
+                                                  @Param("apprStatW")ApprStat apprStatW,
+                                                  @Param("docStatAW")DocStat docStatAW,
+                                                  Pageable pageable);
 
+//======================================================================================================================
 
-    // 내가 결재한 문서 조회
+    // 내 회사의 문서 중 내가 결재한 문서 조회
     // 결재라인에 내가 있고 결재상태가 승인 or 반려인 문서 조회
+    // 내가 결재한 문서 조회 - 내 결재일 기준 최신순 (기본)
     @EntityGraph(attributePaths = {"writer", "documentForm"})
     @Query("SELECT d FROM Document d " +
             "JOIN d.approvalLines al " +
             "WHERE d.company.comId = :comId " +
             "AND al.approver.empId = :empId " +
-            "AND al.apprStat IN (com.multi.mlpenterpriseapprovalsystem.document.enums.ApprStat.A, " +
-            "com.multi.mlpenterpriseapprovalsystem.document.enums.ApprStat.R) " +
+            "AND al.apprStat IN (:apprStatA, :apprStatR) " +
             "ORDER BY al.endedAt DESC")
-    Page<Document> getMyProcessedDocuments(@Param("comId") String comId, @Param("empId") String empId, Pageable pageable);
+    Page<Document> getMyProcessedDocumentsLatest(@Param("comId") String comId,
+                                           @Param("empId") String empId,
+                                           @Param("apprStatA") ApprStat apprStatA,
+                                           @Param("apprStatR") ApprStat apprStatR,
+                                           Pageable pageable);
 
+    // 내가 결재한 문서 조회 - 내 결재일 기준 오래된순
+    @EntityGraph(attributePaths = {"writer", "documentForm"})
+    @Query("SELECT d FROM Document d " +
+            "JOIN d.approvalLines al " +
+            "WHERE d.company.comId = :comId " +
+            "AND al.approver.empId = :empId " +
+            "AND al.apprStat IN (:apprStatA, :apprStatR) " +
+            "ORDER BY al.endedAt ASC")
+    Page<Document> getMyProcessedDocumentsOldest(@Param("comId") String comId,
+                                                 @Param("empId") String empId,
+                                                 @Param("apprStatA") ApprStat apprStatA,
+                                                 @Param("apprStatR") ApprStat apprStatR,
+                                                 Pageable pageable);
+
+    // 내가 결재한 문서 조회 - 특정 내 결재상태 우선 + 내 결재일 기준 최신순
+    @EntityGraph(attributePaths = {"writer", "documentForm"})
+    @Query("SELECT d FROM Document d " +
+            "JOIN d.approvalLines al " +
+            "WHERE d.company.comId = :comId " +
+            "AND al.approver.empId = :empId " +
+            "AND al.apprStat IN (:apprStatA, :apprStatR) " +
+            "ORDER BY CASE WHEN al.apprStat = :myApprStat THEN 0 ELSE 1 END ASC, " +
+            "al.endedAt DESC")
+    Page<Document> getMyProcessedDocumentsByMyApprStat(@Param("comId") String comId,
+                                                       @Param("empId") String empId,
+                                                       @Param("apprStatA") ApprStat apprStatA,
+                                                       @Param("apprStatR") ApprStat apprStatR,
+                                                       @Param("myApprStat") ApprStat myApprStat,
+                                                       Pageable pageable);
+
+//======================================================================================================================
+
+    // 내 회사의 최종승인문서 조회
+    // DocStat이 FI인 문서 모두 조회(최근 결재순(sort=LATEST or sort=NULL), 오래된 결재순(sort=OLDEST))
+    @EntityGraph(attributePaths = {"writer", "documentForm"})
+    @Query("SELECT d FROM Document d " +
+            "WHERE d.company.comId = :comId " +
+            "AND d.temp = false " +
+            "AND d.docStat = :docStatFI")
+    Page<Document> getFinalizedDocuments(@Param("comId")String comId,
+                                         @Param("docStatFI")DocStat docStatFI,
+                                         Pageable pageable);
 }
