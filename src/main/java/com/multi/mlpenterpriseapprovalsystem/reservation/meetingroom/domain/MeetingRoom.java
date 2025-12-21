de677dcfcc3a76cb1785a880533bad63fdd16e86
Songhyun Go <zhthd201@gmail.com>
2025-12-21 17:32:00 +0900

    feat: @Column 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/meetingroom/domain/MeetingRoom.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/meetingroom/domain/MeetingRoom.java
@@ -21,6 +21,7 @@ import lombok.*;
 public class MeetingRoom {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long roomNo;
     @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "com_id", referencedColumnName = "com_id") private Company company;
+    @Column(length = 20, nullable = false)
     private String roomName;
 
     @Column(nullable = false)
