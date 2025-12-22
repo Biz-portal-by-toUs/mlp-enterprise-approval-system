abaf3ae07013e8ab3996b4e63db8ea1068950790
ziyoungkwon <gy011003@naver.com>
2025-12-22 11:16:48 +0900

    feat: 부서 찾을 수 없음 에러코드 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/common/exception/ErrorCode.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/common/exception/ErrorCode.java
@@ -77,6 +77,7 @@ public enum ErrorCode {
     // 부서 관련
     DUPLICATE_DEPID(HttpStatus.CONFLICT, "DUPLICATE_DEPID", "이미 존재하는 부서 코드입니다"),
     DUPLICATE_DEPNAME(HttpStatus.CONFLICT, "DUPLICATE_DEPNAME", "이미 존재하는 부서 이름입니다"),
+    DEPARTMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "DEPARTMENT_NOT_FOUND", "해당 부서를 찾을 수 없습니다"),
 
     // 사원 관련 (기존 USER_NOT_FOUND와 구분하거나 통합하여 사용)
     EMPLOYEE_NOT_FOUND(HttpStatus.NOT_FOUND, "EMPLOYEE_NOT_FOUND", "사원을 찾을 수 없습니다"),
