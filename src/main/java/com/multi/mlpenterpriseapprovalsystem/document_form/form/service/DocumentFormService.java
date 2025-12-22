57a62cf2f928e6903854d58d4f3077eda4f0dece
oJWJo <terry99025@gmail.com>
2025-12-22 14:33:12 +0900

    컨트롤러, 서비스, 양식 상태 규칙 추가

---
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/document_form/form/service/DocumentFormService.java
@@ -0,0 +1,11 @@
+package com.multi.mlpenterpriseapprovalsystem.document_form.form.service;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : 정종원
+ * @filename : DocFormService
+ * @since : 2025-12-22 월요일
+ */
+public interface DocumentFormService {
+}
