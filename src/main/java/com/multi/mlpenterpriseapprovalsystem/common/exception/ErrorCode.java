5640b89529ee7a4b751cb8548c064b2546ae8cef
Lee Ji Heon <jiheon340@gmail.com>
2025-12-23 17:49:03 +0900

    feat: 결재라인 에러 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/common/exception/ErrorCode.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/common/exception/ErrorCode.java
@@ -33,6 +33,9 @@ public enum ErrorCode {
     INVALID_DOCUMENT_STATUS_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_DOCUMENT_STATUS", "잘못된 문서 상태 파라미터 값이 전달되었습니다"),
     INVALID_DOCUMENT_SORT_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_DOCUMENT_SORT_REQUEST", "잘못된 문서 정렬 파라미터 값이 전달되었습니다"),
 
+    // 결재라인 관련
+    INVALID_APPROVAL_LINE_ORDER(HttpStatus.BAD_REQUEST, "INVALID_APPROVAL_LINE_ORDER", "잘못된 결재자 순서입니다"),
+
     // 문서 양식 관련
     DOCUMENT_FORM_NOT_FOUND(HttpStatus.NOT_FOUND, "DOCUMENT_FORM_NOT_FOUND", "문서 양식을 찾을 수 없습니다"),
 
