00985c7d16524d26a99c3353f1fbc27c1725e484
ziyoungkwon <gy011003@naver.com>
2025-12-24 15:29:42 +0900

    feat: hard delete 위해 config에 s3client 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/common/config/S3PresignConfig.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/common/config/S3PresignConfig.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 /**
@@ -17,19 +19,37 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
  */
 @Configuration
 public class S3PresignConfig {
+
     @Bean
-    public S3Presigner s3Presigner(
-            @Value("${app.s3.region}") String region,
+    public AwsCredentialsProvider awsCredentialsProvider(
             @Value("${aws.access-key}") String accessKey,
             @Value("${aws.secret-key}") String secretKey
     ) {
+        return StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(accessKey, secretKey)
+        );
+    }
+
+    @Bean
+    public S3Presigner s3Presigner(
+            @Value("${app.s3.region}") String region,
+            AwsCredentialsProvider awsCredentialsProvider
+    ) {
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        )
-                )
+                .credentialsProvider(awsCredentialsProvider)
+                .build();
+    }
+
+    // ✅ 추가: 서버에서 S3 삭제/조회 등에 쓰는 클라이언트
+    @Bean
+    public S3Client s3Client(
+            @Value("${app.s3.region}") String region,
+            AwsCredentialsProvider awsCredentialsProvider
+    ) {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(awsCredentialsProvider)
                 .build();
     }
 }
