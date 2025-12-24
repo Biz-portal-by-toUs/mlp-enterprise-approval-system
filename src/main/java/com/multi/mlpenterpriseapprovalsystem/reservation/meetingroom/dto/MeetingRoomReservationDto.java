311417a7ed25be800cb5bb9a697308f89af1eb74
Songhyun Go <zhthd201@gmail.com>
2025-12-24 10:39:26 +0900

    feat: 회의실 예약 목록 조회용 Dto 생성

---
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/meetingroom/dto/MeetingRoomReservationDto.java
@@ -0,0 +1,31 @@
+package com.multi.mlpenterpriseapprovalsystem.reservation.meetingroom.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * 회의실 예약 목록 조회를 위한 DTO
+ *
+ * 예약 목록 화면에서 여러 예약을 간단히 표시하기 위해
+ * 필요한 최소 정보만 포함한다.
+ *
+ * @author : 송현님
+ * @filename : ReservationListDto
+ * @since : 2025-12-22 오후 3:21 월요일
+ */
+
+@Getter
+@AllArgsConstructor
+public class MeetingRoomReservationDto {
+    private Long resvNo;
+    private Long roomNo;
+    private String roomName;
+    private LocalDate resvDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String resvEmpId;
+    private String reservedByName;
+}
