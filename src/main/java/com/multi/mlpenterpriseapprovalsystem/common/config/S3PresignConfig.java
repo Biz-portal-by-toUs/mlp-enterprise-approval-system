c12f76f4a42a7f2b608ab36cbcec7f53bdbece15
seunggi-k <fdvc10@gmail.com>
2025-12-24 13:24:20 +0900

    fix : 업로드 버그 수정

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/common/config/S3PresignConfig.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/common/config/S3PresignConfig.java
@@ -1,7 +1,10 @@
 package com.multi.mlpenterpriseapprovalsystem.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
@@ -15,9 +18,18 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3PresignConfig {
     @Bean
-    public S3Presigner s3Presigner() {
+    public S3Presigner s3Presigner(
+            @Value("${app.s3.region}") String region,
+            @Value("${aws.access-key}") String accessKey,
+            @Value("${aws.secret-key}") String secretKey
+    ) {
         return S3Presigner.builder()
-                .region(Region.US_WEST_1) // 버킷 리전과 반드시 동일!
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
                 .build();
     }
 }
