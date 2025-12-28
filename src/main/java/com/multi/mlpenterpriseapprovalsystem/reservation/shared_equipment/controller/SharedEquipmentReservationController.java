a20f90e3ecf31d60145987bf5fa1d999db457bf7
Songhyun Go <zhthd201@gmail.com>
2025-12-28 21:12:55 +0900

    feat: 공유 설비 예약 등록 controller 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/shared_equipment/controller/SharedEquipmentReservationController.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/shared_equipment/controller/SharedEquipmentReservationController.java
@@ -2,6 +2,8 @@ package com.multi.mlpenterpriseapprovalsystem.reservation.shared_equipment.contr
 
 import com.multi.mlpenterpriseapprovalsystem.auth.dto.CustomUser;
 import com.multi.mlpenterpriseapprovalsystem.common.ResponseDto;
+
+import com.multi.mlpenterpriseapprovalsystem.reservation.shared_equipment.dto.ReqReservationCreateDto;
 import com.multi.mlpenterpriseapprovalsystem.reservation.shared_equipment.dto.ResReservationListDto;
 import com.multi.mlpenterpriseapprovalsystem.reservation.shared_equipment.service.SharedEquipmentReservationService;
 import lombok.RequiredArgsConstructor;
@@ -9,16 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * 공유 설비 예약 API Controller
- * <p>
+
  * 공유 설비 예약과 관련된 사용자 행위(예약 조회·생성·취소)를 처리하는
  * REST API 전용 Controller이다.
  *
@@ -47,9 +46,13 @@ public class SharedEquipmentReservationController {
                 .status(HttpStatus.OK)
                 .body(new ResponseDto<>(HttpStatus.OK, null, reservations));
 
+    }
 
-
-
+    @PostMapping("/shared-equipment-reservations")
+    public ResponseEntity<ResponseDto<List<ResReservationListDto>>> createReservation(@RequestBody ReqReservationCreateDto dto,
+                                                                                      @AuthenticationPrincipal CustomUser user) {
+        sharedEquipmentReservationService.createReservation(dto, user);
+        return ResponseEntity.ok().build();
     }
 }
 
