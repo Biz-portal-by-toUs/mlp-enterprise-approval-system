e735b3838e427dcfaa6934f9c939f868672754aa
Songhyun Go <zhthd201@gmail.com>
2025-12-28 14:36:45 +0900

    feat: 법인 차량 예약 등록 controller 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/corporate_car/controller/CorporateCarReservationController.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/corporate_car/controller/CorporateCarReservationController.java
@@ -3,17 +3,16 @@ package com.multi.mlpenterpriseapprovalsystem.reservation.corporate_car.controll
 import com.multi.mlpenterpriseapprovalsystem.auth.dto.CustomUser;
 import com.multi.mlpenterpriseapprovalsystem.common.ResponseDto;
 
+import com.multi.mlpenterpriseapprovalsystem.reservation.corporate_car.dto.ReqReservationCreateDto;
 import com.multi.mlpenterpriseapprovalsystem.reservation.corporate_car.dto.ResReservationListDto;
 import com.multi.mlpenterpriseapprovalsystem.reservation.corporate_car.service.CorporateCarReservationService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,6 +45,13 @@ public class CorporateCarReservationController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseDto<>(HttpStatus.OK, null, reservations));
+    }
+
+    @PostMapping("/corporate-car-reservations")
+    public ResponseEntity<ResponseDto<List<ResReservationListDto>>> createReservation(@RequestBody ReqReservationCreateDto dto,
+                                                                                      @AuthenticationPrincipal CustomUser user) {
+        corporateCarReservationService.createReservation(dto, user);
+        return ResponseEntity.ok().build();
     }
 }
 
