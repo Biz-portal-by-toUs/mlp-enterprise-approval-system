4c92db691cf17376c72e50f08c3b91b8ebcb9451
Songhyun Go <zhthd201@gmail.com>
2025-12-28 18:11:27 +0900

    feat: 공유 설비에 필요한 Dto 생성

---
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/shared_equipment/dto/ReqReservationCreateDto.java
@@ -0,0 +1,25 @@
+package com.multi.mlpenterpriseapprovalsystem.reservation.shared_equipment.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : 송현님
+ * @filename : MeetingRoomReservationCreateDto
+ * @since : 2025-12-24 오후 1:19 수요일
+ */
+
+@Getter
+public class ReqReservationCreateDto {
+
+    private Long eqNo;
+    private LocalDate resvDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String purp;
+
+}
