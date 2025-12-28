d2515baa2a3dd83e567a79661833fd40445d35e8
Songhyun Go <zhthd201@gmail.com>
2025-12-28 18:11:04 +0900

    feat: Dto 설명 수정

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/corporate_car/dto/ResReservationTimeDto.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/reservation/corporate_car/dto/ResReservationTimeDto.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
  * 법인 차량 예약 타임라인 표시를 위한 DTO
  *
  * 예약의 시작 시간과 종료 시간 정보를 제공하여
- * 회의실 예약 타임라인(시간 막대)을 그리는 데 사용된다.
+ * 법인 차량 예약 타임라인(시간 막대)을 그리는 데 사용된다.
  *
  * @author : 송현님
  * @filename : ReservationTimeDto
