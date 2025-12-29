062a76fb220553c49ec8b284f173201e8a02e2d7
ziyoungkwon <gy011003@naver.com>
2025-12-30 02:53:14 +0900

    refactor: getter 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/auth/dto/CustomUser.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/auth/dto/CustomUser.java
@@ -1,10 +1,7 @@
 package com.multi.mlpenterpriseapprovalsystem.auth.dto;
 
 import com.multi.mlpenterpriseapprovalsystem.common.enums.TokenSubjectType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -21,6 +18,7 @@ import java.util.Collection;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
 public class CustomUser implements UserDetails {
 
     private Long subjectId;                 // comNo or empNo
