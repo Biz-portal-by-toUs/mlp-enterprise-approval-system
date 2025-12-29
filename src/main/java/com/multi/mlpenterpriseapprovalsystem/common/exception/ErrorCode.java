725736f2f7770f7891e57c7c16d8e66197e4d95c
ziyoungkwon <gy011003@naver.com>
2025-12-29 13:00:23 +0900

    feat: 에러코드 추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/common/exception/ErrorCode.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/common/exception/ErrorCode.java
@@ -24,6 +24,7 @@ public enum ErrorCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "로그인이 필요합니다"),
     ALREADY_USE_PASSWORD(HttpStatus.BAD_REQUEST, "ALREADY_USE_PASSWORD", "이미 사용중인 비밀번호입니다"),
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "INVALID_INPUT", "입력정보가 일치하지 않습니다"),
+    NOT_BLANK(HttpStatus.BAD_REQUEST, "NOT_BLANK", "입력이 필수인 칸 입니다"),
 
     //요금제 관련
     SUBSCRIPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "SUBSCRIPTION_NOT_FOUND", "요금제를 찾을 수 없습니다"),
