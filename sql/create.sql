02f71f5f6d052db2376f7a0e40f45a367e154f9d
ziyoungkwon <gy011003@naver.com>
2025-12-31 13:23:37 +0900

    feat: schedule, emp_schedule 테이블에 allDay 컬럼 추가

--- sql/create.sql
+++ sql/create.sql
@@ -969,7 +969,13 @@ ALTER TABLE employee MODIFY COLUMN object_key VARCHAR(255) NULL;
 
 
 -- 일정 관련 변경 --
+use bizportal;
 -- 1) 부서+회사 스케줄 테이블에서 dep_no nullable로 변경
 ALTER TABLE schedule
     MODIFY dep_no BIGINT NULL;
+-- 2) emp_schedule, schedule all_day 컬럼 추가
+ALTER TABLE emp_schedule
+    ADD COLUMN all_day TINYINT(1) NOT NULL DEFAULT 0 AFTER ended_at;
 
+ALTER TABLE schedule
+    ADD COLUMN all_day TINYINT(1) NOT NULL DEFAULT 0 AFTER ended_at;
