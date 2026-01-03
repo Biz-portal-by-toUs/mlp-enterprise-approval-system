5a24a60f040bb76a013a2d471343def517f30e9f
Lee Ji Heon <jiheon340@gmail.com>
2026-01-03 22:10:02 +0900

    refactor: 코드 배치 변경

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/payment/domain/PaymentHistory.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/payment/domain/PaymentHistory.java
@@ -2,9 +2,7 @@ package com.multi.mlpenterpriseapprovalsystem.payment.domain;
 
 import com.multi.mlpenterpriseapprovalsystem.company.domain.Company;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.math.BigDecimal;
@@ -13,19 +11,37 @@ import java.time.LocalDateTime;
 /**
  * 결제 내역 엔티티
  *
- * @author : 김승기
+ * @author : 이지헌
  * @filename : PaymentHistory
  * @since : 2025. 12. 16. 화요일
  */
 @Entity @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "payment_history")
 public class PaymentHistory {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long payhNo;
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "com_id", referencedColumnName = "com_id") private Company company;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long payhNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "com_id", referencedColumnName = "com_id")
+    private Company company;
+
     private BigDecimal amount;
     private Boolean payResult;
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "paym_no") private PaymentMethod paymentMethod;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paym_no")
+    private PaymentMethod paymentMethod;
+
     @CreatedDate
     private LocalDateTime createdAt;
+
+    public PaymentHistory(Company company, BigDecimal subPrice, boolean payResult, PaymentMethod paymentMethod) {
+        this.company = company;
+        this.amount = subPrice;
+        this.payResult = payResult;
+        this.paymentMethod = paymentMethod;
+    }
 }
