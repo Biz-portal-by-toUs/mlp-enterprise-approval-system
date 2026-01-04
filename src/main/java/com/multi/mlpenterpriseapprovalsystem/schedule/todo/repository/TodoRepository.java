ccf65f206aa9923a205134434e0563c6dd973120
ziyoungkwon <gy011003@naver.com>
2026-01-04 20:42:00 +0900

    feat: 투두리스트 찾는 레포지토리

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/schedule/todo/repository/TodoRepository.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/schedule/todo/repository/TodoRepository.java
@@ -3,6 +3,8 @@ package com.multi.mlpenterpriseapprovalsystem.schedule.todo.repository;
 import com.multi.mlpenterpriseapprovalsystem.schedule.todo.domain.TodoList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * 투두리스트 레포지토리
  *
@@ -11,4 +13,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @since : 2026. 1. 4. 일요일
  */
 public interface TodoRepository extends JpaRepository<TodoList, Long> {
+
+    Optional<TodoList> findByTodoNoAndEmployeeEmpId(Long todoNo, String empId);
 }
