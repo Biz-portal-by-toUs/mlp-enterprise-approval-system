b78af18254d58335190300dbc78a545561d428ab
ziyoungkwon <gy011003@naver.com>
2026-01-04 20:26:20 +0900

    feat: 투두리스트 등록 서비스

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/schedule/todo/service/TodoService.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/schedule/todo/service/TodoService.java
@@ -1,5 +1,13 @@
 package com.multi.mlpenterpriseapprovalsystem.schedule.todo.service;
 
+import com.multi.mlpenterpriseapprovalsystem.common.exception.CustomException;
+import com.multi.mlpenterpriseapprovalsystem.common.exception.ErrorCode;
+import com.multi.mlpenterpriseapprovalsystem.employee.domain.Employee;
+import com.multi.mlpenterpriseapprovalsystem.employee.repository.EmployeeRepository;
+import com.multi.mlpenterpriseapprovalsystem.schedule.todo.domain.TodoList;
+import com.multi.mlpenterpriseapprovalsystem.schedule.todo.dto.ReqCreateTodoDto;
+import com.multi.mlpenterpriseapprovalsystem.schedule.todo.dto.ResTodoDto;
+import com.multi.mlpenterpriseapprovalsystem.schedule.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,4 +23,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TodoService {
+
+    private final EmployeeRepository employeeRepository;
+    private final TodoRepository todoRepository;
+
+    @Transactional
+    public ResTodoDto create(String empId, ReqCreateTodoDto req) {
+        Employee employee = employeeRepository.findByEmpId(empId)
+                .orElseThrow(() -> new CustomException(ErrorCode.EMPLOYEE_NOT_FOUND));
+
+        TodoList todo = TodoList.create(employee, req.getTitle());
+
+
+        TodoList saved = todoRepository.save(todo);
+        return ResTodoDto.from(saved);
+    }
 }
