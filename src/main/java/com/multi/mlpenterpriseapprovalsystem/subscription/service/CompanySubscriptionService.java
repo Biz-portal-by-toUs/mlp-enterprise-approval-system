53ae2b510f171a5e0f87a7e8138fa208c2d8248d
Lee Ji Heon <jiheon340@gmail.com>
2026-01-04 21:14:19 +0900

    feat: 내 회사의 현재 구독 정보 조회 구현

---
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/subscription/service/CompanySubscriptionService.java
@@ -0,0 +1,40 @@
+package com.multi.mlpenterpriseapprovalsystem.subscription.service;
+
+import com.multi.mlpenterpriseapprovalsystem.common.exception.CustomException;
+import com.multi.mlpenterpriseapprovalsystem.common.exception.ErrorCode;
+import com.multi.mlpenterpriseapprovalsystem.subscription.domain.CompanySubscription;
+import com.multi.mlpenterpriseapprovalsystem.subscription.dto.response.ResCompanySubscriptionDto;
+import com.multi.mlpenterpriseapprovalsystem.subscription.repository.CompanySubscriptionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : 이지헌
+ * @filename : CompanySubscriptionService
+ * @since : 26. 1. 4. 일요일
+ */
+
+@Service
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
+public class CompanySubscriptionService {
+    private final CompanySubscriptionRepository companySubscriptionRepository;
+
+    public ResCompanySubscriptionDto getCurrentSubscription(String comId) {
+        CompanySubscription sub = companySubscriptionRepository.findByCompany_ComId(comId)
+                .orElseThrow(() -> new CustomException(ErrorCode.SUBSCRIPTION_NOT_FOUND));
+
+        return ResCompanySubscriptionDto.builder()
+                .subNo(sub.getSubscription().getSubNo()) // 현재 연결된 요금제 번호
+                .subName(sub.getSubscription().getSubName())
+                .status(sub.getStatus())
+                .nextBillingDate(sub.getNextBillingDate())
+                .autoRenewal(sub.isAutoRenewal())
+                .build();
+    }
+}
