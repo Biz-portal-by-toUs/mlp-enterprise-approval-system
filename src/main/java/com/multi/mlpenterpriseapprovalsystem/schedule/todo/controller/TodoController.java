635378950dcedd5ba0921181a644dc12847484a2
ziyoungkwon <gy011003@naver.com>
2026-01-04 20:26:15 +0900

    feat: 투두리스트 등록 컨트롤러

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/schedule/todo/controller/TodoController.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/schedule/todo/controller/TodoController.java
@@ -1,8 +1,18 @@
 package com.multi.mlpenterpriseapprovalsystem.schedule.todo.controller;
 
+import com.multi.mlpenterpriseapprovalsystem.auth.dto.CustomUser;
+import com.multi.mlpenterpriseapprovalsystem.common.ResponseDto;
+import com.multi.mlpenterpriseapprovalsystem.schedule.todo.dto.ReqCreateTodoDto;
+import com.multi.mlpenterpriseapprovalsystem.schedule.todo.dto.ResTodoDto;
+import com.multi.mlpenterpriseapprovalsystem.schedule.todo.service.TodoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 /**
  * 투두리스트 컨트롤러
@@ -15,4 +25,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/todo")
 public class TodoController {
+
+    private final TodoService todoService;
+
+    @PostMapping
+    public ResponseEntity<ResponseDto<ResTodoDto>> create(
+            @AuthenticationPrincipal CustomUser user,
+            @RequestBody @Valid ReqCreateTodoDto req
+    ) {
+
+        ResTodoDto created = todoService.create(user.getUsername(), req);
+
+        URI location = URI.create("/api/v1/todos/" + created.getTodoNo());
+
+        return ResponseEntity
+                .created(location) // ✅ 201 + Location
+                .body(new ResponseDto<>(HttpStatus.CREATED, "투두 생성 완료", created));
+    }
 }
