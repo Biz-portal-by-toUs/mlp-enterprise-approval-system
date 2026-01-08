38f117cab11332611e7012993b4880821b7dd7f9
seunggi-k <fdvc10@gmail.com>
2026-01-08 10:34:11 +0900

    feat : 임베딩 완료시 알림 발송 ispublic이 true일때만 전송

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/provdocument/service/ProvDocumentService.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/provdocument/service/ProvDocumentService.java
@@ -199,8 +199,10 @@ public class ProvDocumentService {
         String comId=doc.getCompany().getComId();
 
         List<Employee> emps = employeeRepository.findAllByCompany_ComId(comId);
-        for(Employee emp : emps) {
-            notificationService.sendNotification(emp, NotificationType.OTHER, "챗봇 사용 가능 알림", "이제부터\""+doc.getDocTitle()+"\" 에 대한 질의를 챗봇에서 사용할 수 있습니다.","/employees");
+        if (doc.getIsPublic()==Boolean.TRUE){
+            for(Employee emp : emps) {
+                notificationService.sendNotification(emp, NotificationType.OTHER, "챗봇 사용 가능 알림", "이제부터\""+doc.getDocTitle()+"\" 에 대한 질의를 챗봇에서 사용할 수 있습니다.","/employees");
+            }
         }
         return doc.getProvNo();
     }
