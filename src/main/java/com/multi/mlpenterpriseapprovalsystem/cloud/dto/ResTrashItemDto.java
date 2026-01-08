0e1a1aad5cfea6364527eda25bc15d47cb1e4927
Songhyun Go <zhthd201@gmail.com>
2026-01-08 11:34:45 +0900

    feat: Dto 이름 변경

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/cloud/dto/ResTrashItemDto.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/cloud/dto/ResTrashItemDto.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 /**
- * 휴지통 Dto
+ * 휴지통 목록 조회 API의 응답 항목 DTO 입니다.
+
+ * 휴지통에 존재하는 대상(폴더/파일)의 공통 정보를 하나의 모델로 표현합니다.
+ * type이 FOLDER인 경우: size/contentType은 null
+ * type이 FILE인 경우: size/contentType이 채워질 수 있음
  *
  * @author : 송현님
  * @filename : TrashItemDto
@@ -15,7 +19,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
-public class TrashItemDto {
+public class ResTrashItemDto {
     private String type;          // "FOLDER" | "FILE"
     private Long id;              // folderNo or attachmentId
     private String name;          // folderName or originalName
