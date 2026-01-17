bb3cab64bc291bf4933170b0b0d67263c2763d08
seunggi-k <fdvc10@gmail.com>
2026-01-17 17:22:58 +0900

    feat : 알림 비동기 전송 구현

---
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/provdocument/service/ProvNotificationService.java
@@ -0,0 +1,43 @@
+package com.multi.mlpenterpriseapprovalsystem.provdocument.service;
+
+import com.multi.mlpenterpriseapprovalsystem.employee.domain.Employee;
+import com.multi.mlpenterpriseapprovalsystem.employee.repository.EmployeeRepository;
+import com.multi.mlpenterpriseapprovalsystem.notification.domain.NotificationType;
+import com.multi.mlpenterpriseapprovalsystem.notification.service.NotificationService;
+import com.multi.mlpenterpriseapprovalsystem.provdocument.event.ProvDocumentApprovedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.List;
+
+/**
+ * 알림 비동기 전송 서비스
+ *
+ * @author : 김승기
+ * @filename : ProvNotificationService
+ * @since : 2026. 1. 17. 토요일
+ */
+@Service
+@RequiredArgsConstructor
+public class ProvNotificationService {
+    private final NotificationService notificationService;
+    private final EmployeeRepository employeeRepository;
+
+    @Async("taskExecutor")
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleDocApprovedEvent(ProvDocumentApprovedEvent event) {
+        List<Employee> emps = employeeRepository.findAllByCompany_ComId(event.comId());
+
+        for (Employee emp : emps) {
+            notificationService.sendNotification(
+                    emp, NotificationType.OTHER,
+                    "챗봇 사용 가능 알림",
+                    "이제부터 \"" + event.docTitle() + "\" 에 대한 질의를 챗봇에서 사용할 수 있습니다.",
+                    "/employees"
+            );
+        }
+    }
+}
