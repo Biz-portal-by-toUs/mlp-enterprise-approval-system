e48115a66e4bb5d6b0028e131e6909bef667928f
seunggi-k <fdvc10@gmail.com>
2026-01-17 17:41:18 +0900

    feat : enableasync추가

--- src/main/java/com/multi/mlpenterpriseapprovalsystem/MlpEnterpriseApprovalSystemApplication.java
+++ src/main/java/com/multi/mlpenterpriseapprovalsystem/MlpEnterpriseApprovalSystemApplication.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
@@ -12,6 +13,7 @@ import java.util.TimeZone;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableAsync
 public class MlpEnterpriseApprovalSystemApplication {
 
     @PostConstruct
